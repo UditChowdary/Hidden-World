e65a97937b095e3f63a6464423738a3d6d1d0590
Udit Chowdary Jasti <86521079+UditChowdary@users.noreply.github.com>
2025-12-02 15:27:56 -0500

    Add files via upload

---
+++ includes/security.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include('dbconfig.php');
+if($connection)
+{
+    // echo "Database Connected";
+}
+else
+{
+    header("Location: dbconfig.php");
+}
---
+++ states/Andhra.php
@@ -0,0 +1,55 @@
+<?php require_once '../header.php';
+use App\classes\Helper;
+?>
+
+
+<!-- start of the carasoule -->
+<br><br>
+<center>
+    
+<div class="row">
+
+<div class="" style="padding:10px;">
+<center>
+<a href="karnataka.php">
+<img src="assets/images/Andhra Pradesh.jpg" style="border-radius: 50%; width:200px;" alt="">
+<h6>Famouse Places</h6>
+</a>
+</center>
+</div>
+
+
+<div class="" style="padding:10px;">
+<center>
+<a href="#">
+<img src="assets/images/question.jpg" style="border-radius: 50%; width:200px;" alt="">
+<h6>Hidden Places</h6>
+</a>
+</center>
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+<br><br>
+</center>
+<!-- end of the carasoule -->
+                <div class="row">
+
+
+
+
+
+
+
+                <?php
+require_once 'footer.php';
+?>
+
